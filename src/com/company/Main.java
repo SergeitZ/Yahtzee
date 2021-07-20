ee877523bfc81f79e9cbf77a31220db2b96b61bc
SergeitZ <sergiofranco6@gmail.com>
2021-07-19 20:52:55 -0400

    Added HighLow class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,42 +4,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        Cup myCup = new Cup();
         Scanner scanner = new Scanner(System.in);
-        Die die1 = new Die();
-        Die die2 = new Die();
+        String input;
 
-        boolean didWin = true;
-
-        while (didWin) {
-            die1.roll();
-            die2.roll();
-            int total = die1.faceUpValue + die2.faceUpValue;
-
-            System.out.println(die1.faceUpValue + " + " + die2.faceUpValue + " = " + (total));
-            System.out.println("Will the next number be (h)igher or (l)ower?");
-            String input = scanner.next();
-
-            die1.roll();
-            die2.roll();
-            int newTotal = die1.faceUpValue + die2.faceUpValue;
-            if (total == newTotal) {
-                System.out.println("same number you lose");
-            }
-
-            boolean isLarger = newTotal > total;
-            System.out.println(die1.faceUpValue + " + " + die2.faceUpValue + " = " + (newTotal));
-            System.out.println(isLarger ? "Number is higher" : "Number is lower");
-            if (input.equals("l")) { //guessed higher
-                if (isLarger || newTotal == total) {
-                    didWin = false;
-                }
-            } else { //guessed lower
-                if (!isLarger) {
-                    didWin = false;
-                }
-            }
-            System.out.println(didWin ? "you WIN" : "You LOSE");
-        }
-        scanner.close();
+        myCup.roll();
+        System.out.println(myCup.displayCup());
+        System.out.println("Select dice do you want to re-roll (1-5)");
+        input = scanner.nextLine();
+        myCup.roll(myCup.parseSelections(input));
+        System.out.println(myCup.displayCup());
     }
 }
