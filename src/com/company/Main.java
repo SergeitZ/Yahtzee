19f22d7adb6eac1cca2b5cf5a1efe08bd1b1825b
SergeitZ <sergiofranco6@gmail.com>
2021-07-20 20:13:25 -0400

    Yahtzee and player classes added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,12 @@
 package com.company;
-import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Cup myCup = new Cup();
-        Scanner scanner = new Scanner(System.in);
-        String input;
+        Yahtzee game = new Yahtzee();
+        game.play();
 
-        myCup.roll();
-        System.out.println(myCup.displayCup());
-        System.out.println("Select dice do you want to re-roll (1-5)");
-        input = scanner.nextLine();
-        myCup.roll(myCup.parseSelections(input));
-        System.out.println(myCup.displayCup());
     }
 }
